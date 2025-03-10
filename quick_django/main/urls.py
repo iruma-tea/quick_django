596b253c44a43301ac97efbbb84a157c40a1cb15
shusuke-iruma-tea <shusuke.pana@gmail.com>
2025-03-10 12:39:02 +0900

    booksの一覧画面を作成

--- quick_django/main/urls.py
+++ quick_django/main/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('temp', views.temp, name='temp'),
+    path('list', views.list, name='list'),
 ]
--- quick_django/main/views.py
+++ quick_django/main/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Book
 
 # Create your views here.
 
@@ -13,3 +14,9 @@ def temp(request):
         'msg': 'こんにちは、世界！'
     }
     return render(request, 'main/temp.html', context)
+
+
+def list(request):
+    # すべての書籍情報を取得する
+    books = Book.objects.all()
+    return render(request, 'main/list.html', {'books': books})
